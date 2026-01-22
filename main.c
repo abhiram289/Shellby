9286c2446942a22ffbdad91df1ab151f39e13542
abhiram289 <abhirammns28@gmail.com>
2026-01-22 22:16:07 +0530

    Basic shell prompt and input handling

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_INPUT 1024
+
+int main() {
+    char input[MAX_INPUT];
+
+    while (1) {
+        printf("myshell> ");
+        fflush(stdout);
+
+        if (!fgets(input, MAX_INPUT, stdin)) {
+            break;
+        }
+
+        input[strcspn(input, "\n")] = 0;
+
+        if (strcmp(input, "exit") == 0) {
+            break;
+        }
+
+        printf("You typed: %s\n", input);
+    }
+
+    return 0;
+}

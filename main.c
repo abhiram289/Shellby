5808010de828b8ab2c8e1554deb42e7e618b20d1
abhiram289 <abhirammns28@gmail.com>
2026-01-22 22:56:07 +0530

    Added command execution with fork, execvp, cd, exit.

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #define MAX_INPUT 1024
+#define MAX_ARGS 64
 
 int main() {
     char input[MAX_INPUT];
+    char *args[MAX_ARGS];
 
     while (1) {
         printf("myshell> ");
@@ -17,11 +21,49 @@ int main() {
 
         input[strcspn(input, "\n")] = 0;
 
-        if (strcmp(input, "exit") == 0) {
-            break;
+        if (strlen(input) == 0) {
+            continue;
         }
 
-        printf("You typed: %s\n", input);
+        int i=0;
+	char *token = strtok(input, " ");
+
+	while (token!= NULL && i < MAX_ARGS -1) {
+	    args[i++] = token;
+	    token = strtok(NULL, " ");
+	} 
+	// loops over all words then stores pointers in args[]
+	args[i] = NULL;
+	
+	if (strcmp(args[0], "exit") == 0) {
+	    break;
+	}
+
+	if (strcmp(args[0], "cd") == 0) {
+	    if (args[1] == NULL) {
+                fprintf(stderr, "myshell: expected argument to \"cd\"\n");
+            } else {
+                if (chdir(args[1]) != 0) {
+                    perror("myshell");
+                }
+            }
+            continue;
+        }
+
+	pid_t pid = fork();
+
+	if(pid == 0) {
+	    execvp(args[0], args);
+	    perror("myshell");
+	    exit(EXIT_FAILURE);
+	}
+	else if (pid < 0) {
+	    perror("myerror");
+	}
+	else {
+	    wait(NULL);
+	}
+ 
     }
 
     return 0;
